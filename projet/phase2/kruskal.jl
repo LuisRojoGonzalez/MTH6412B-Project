51896509d75917f0149777ad90de65a2b81b84f8
LuisRojoGonzalez <luis-antonio.rojo-gonzalez@polymtl.ca>
2022-10-16 01:19:07 -0400

    It initializes the branch of phase 2.

---
+++ projet/phase2/kruskal.jl
@@ -0,0 +1,8 @@
+"""
+This script contains the kruskal algorithm to create a minimum spanning tree of a undirected graph.
+"""
+
+# the following implementation is based on pseudo-code in Introcution to algorithms (3 eds.) section 23.2
+# pp. 631
+
+# creates the function make
