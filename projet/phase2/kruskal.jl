8993f577da45c3aa33292e022e16fc086c207a56
LuisRojoGonzalez <luis-antonio.rojo-gonzalez@polymtl.ca>
2022-10-16 03:19:10 -0400

    This version incluides the script of kruskal algorithm for a minimum spanning tree through the implementation of the pseudo-code that appears on the CLRS (Introduction to algorithms, 3eds.) pp. 631.

--- projet/phase2/kruskal.jl
+++ projet/phase2/kruskal.jl
@@ -1,3 +1,5 @@
+import Base
+
 """
 This script contains the kruskal algorithm to create a minimum spanning tree of a undirected graph.
 """
@@ -5,4 +7,43 @@ This script contains the kruskal algorithm to create a minimum spanning tree of 
 # the following implementation is based on pseudo-code in Introcution to algorithms (3 eds.) section 23.2
 # pp. 631
 
-# creates the function make
+function kruskal(g::Graph{T})
+    # we create the MST object
+    A = []
+    
+    # initializes the sum of the weights
+    total = 0
+    
+    # initialize the set of trees
+    tree_set = [[i] for i in 1:length(g["Edges_m"])]
+    
+    # it gets the index in which ordering the edges
+    sortindex = sortperm(g["Weights"])
+    
+    # initializes the counter
+    index = 0
+    
+    # we know that there are going to be |V|-1 edges
+    while length(A) < length(g["Edges_m"]) - 1
+        index += 1
+        # gets the edge
+        edge = g["Edges_v"][sortindex[index]] # replace sortindex[1] with the counter in the loop
+        u, v = edge[1], edge[2]
+        
+        # checks whether the trees are the same
+        if !isequal(tree_set[u], tree_set[v])
+            # adds the edge to the MST and its corresponding weight w
+            w = g["Weights"][sortindex[index]]
+            push!(A, [u,v,w])
+            
+            # makes the union of the sets
+            push!(tree_set[u], v)
+            push!(tree_set[v], u)
+            
+            # updates the sum of the weights
+            total = total + w
+        end
+    end
+    # it returns the MST construction and the total distance in it
+    return Dict("MST" => A, "Distance" => total)
+end
