c306714baede8556ce8840350103f524ff33f482
Luis Rojo-Gonzalez <luis-antonio.rojo-gonzalez@polymtl.ca>
2022-10-27 18:22:11 -0400

    Changes the way the kruskal function returns the results.

--- projet/phase2/kruskal.jl
+++ projet/phase2/kruskal.jl
@@ -7,23 +7,22 @@ import Base
 This script contains the kruskal algorithm to create a minimum spanning tree of a undirected graph. The following
 implementation is based on pseudo-code in Introcution to algorithms (3 eds.) section 23.2 pp. 631.
 """
-function kruskal(g)#(g::Graph{T}) where T
+function kruskal(graph)#(g::Graph{T}) where T
     
     # initialize the MST object
-    mst = []
+    mstEdges = []
 
     # initialize the distance or cost of the minimum spanning tree (MST)
-    mst_cost = 0
+    mstWeights = []
 
     # it gets the index in which ordering the edges
-    sortindex = sortperm(g["Weights"])
+    sortindex = sortperm(graph["Weights"])
 
     # creates the list of parents
-    parent = [i for i in 1:length(g["Nodes"])]
+    parent = [i for i in 1:length(graph["Nodes"])]
 
     # recursive function to find where the node comes from
     function trace_parent(i)
-        counter = 0
         if i != parent[i]
             parent[i] = trace_parent(parent[i])
         end
@@ -33,19 +32,19 @@ function kruskal(g)#(g::Graph{T}) where T
     for index in sortindex
 
         # gets the parent of the origin node
-        parent_a = trace_parent(g["Edges_v"][index][1][1])
+        parent_a = trace_parent(graph["Edges_v"][index][1][1])
 
         # gets the parent of the destination node
-        parent_b = trace_parent(g["Edges_v"][index][2][1])
+        parent_b = trace_parent(graph["Edges_v"][index][2][1])
 
         # if they belong to different trees then
         if parent_a != parent_b
 
-            # add the cost to the current MST
-            mst_cost += g["Weights"][index]
-
             # update the tree by adding the edge
-            push!(mst, g["Edges_v"][index])
+            push!(mstEdges, graph["Edges_v"][index])
+
+            # add the cost to the current MST
+            push!(mstWeights, graph["Weights"][index])
 
             # update the list of parents
             parent[parent_a] = parent_b
@@ -54,6 +53,5 @@ function kruskal(g)#(g::Graph{T}) where T
     
     end
 
-    return Dict("MST" => mst, "Distance" => mst_cost)
-
+    return Dict("MST" => mstEdges, "Distance" => mstWeights)
 end
